7deb93d534cf593e0bf730344afaac0eb3c0d632
Jolene <joleneodia@hotmail.com>
2024-10-18 19:11:12 -0400

    Update credentials.js

--- CART-211/DabL/js/credentials.js
+++ CART-211/DabL/js/credentials.js
@@ -1,9 +1,9 @@
 //Firebase configuration
 export const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID,
+    apiKey: env.API_KEY,
+    authDomain: env.AUTH_DOMAIN,
+    projectId: env.PROJECT_ID,
+    storageBucket: env.STORAGE_BUCKET,
+    messagingSenderId: env.MESSAGING_SENDER_ID,
+    appId: env.APP_ID,
 };
