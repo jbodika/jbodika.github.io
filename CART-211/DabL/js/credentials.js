80de10167e0d93490aafd049e495fbe0af1b1580
Jolene <95239733+jbodika@users.noreply.github.com>
2024-10-18 20:29:24 -0400

    Update credentials.js

--- CART-211/DabL/js/credentials.js
+++ CART-211/DabL/js/credentials.js
@@ -1,9 +1,10 @@
 //Firebase configuration
 export const firebaseConfig = {
-    apiKey: env.API_KEY,
-    authDomain: env.AUTH_DOMAIN,
-    projectId: env.PROJECT_ID,
-    storageBucket: env.STORAGE_BUCKET,
-    messagingSenderId: env.MESSAGING_SENDER_ID,
-    appId: env.APP_ID,
+    apiKey: "AIzaSyDCNbOigX073RJrRWWUPE9tUH-52PJKcW4",
+    authDomain: "dabl-95a47.firebaseapp.com",
+    databaseURL: "https://dabl-95a47-default-rtdb.firebaseio.com",
+    projectId: "dabl-95a47",
+    storageBucket: "dabl-95a47.appspot.com",
+    messagingSenderId: "1086495030772",
+    appId: "1:1086495030772:web:9f81ef078a0758b18ccda1"
 };
