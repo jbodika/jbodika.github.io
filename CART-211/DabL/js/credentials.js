5dc722ec1794523cdd385493c3fb85e6f7d898d4
Jolene <95239733+jbodika@users.noreply.github.com>
2024-10-18 21:36:18 -0400

    Update credentials.js

--- CART-211/DabL/js/credentials.js
+++ CART-211/DabL/js/credentials.js
@@ -1,1 +1,9 @@
-
+export const firebaseConfig = {
+    apiKey: "AIzaSyDCNbOigX073RJrRWWUPE9tUH-52PJKcW4",
+    authDomain: "dabl-95a47.firebaseapp.com",
+    databaseURL: "https://dabl-95a47-default-rtdb.firebaseio.com",
+    projectId: "dabl-95a47",
+    storageBucket: "dabl-95a47.appspot.com",
+    messagingSenderId: "1086495030772",
+    appId: "1:1086495030772:web:9f81ef078a0758b18ccda1"
+};
